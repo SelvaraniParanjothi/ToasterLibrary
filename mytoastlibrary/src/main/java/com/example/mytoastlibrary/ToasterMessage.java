e4781fcee4c8ff808519b0a77f279a868c6a83b5
SelvaraniParanjothi <selvaparanjothi15@gmail.com>
2021-10-22 15:51:29 +0530

    first push

---
+++ mytoastlibrary/src/main/java/com/example/mytoastlibrary/ToasterMessage.java
@@ -0,0 +1,19 @@
+package com.example.mytoastlibrary;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
